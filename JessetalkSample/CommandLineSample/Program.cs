5b7af94c51aa8bf142412f7f89cf9d6114829912
guilinhd <394811364@qq.com>
2022-01-16 22:32:34 +0800

    增加的commandline的demo

---
+++ JessetalkSample/CommandLineSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace CommandLineSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddCommandLine(args);
+
+            IConfigurationRoot configuration = builder.Build();
+            Console.WriteLine($"name is:{configuration["name"]}");
+            Console.WriteLine($"age is:{configuration["age"]}");
+
+            Console.ReadKey();
+        }
+    }
+}
